5e7b43dd30d5676632b25f9ae1ae958a850efdbe
Lana Ivko <lana.ivko@inforeachinc.com>
2025-03-20 11:55:47 +0200

    add fake midi generator

--- src/main/java/ivko/lana/neurotone/util/FakeMidiCreator.java
+++ src/main/java/ivko/lana/neurotone/util/FakeMidiCreator.java
@@ -12,14 +12,14 @@ public class FakeMidiCreator
 {
     private static final int BUFFER_SIZE = 8192;
     private static final int QUEUE_CAPACITY = 10;
-    public static final String BASE_DIRECTORY = "F:\\MUSIC\\music2\\";
+    public static final String BASE_DIRECTORY = "F:\\MUSIC\\music4\\";
     public static final String OUTPUT_MID = BASE_DIRECTORY + "output.mid";
     public static final String INPUT_WAV = BASE_DIRECTORY + "audio.wav";
     private static final String LOG_FILE = BASE_DIRECTORY + "log.txt";
     private static final double NOTES_PER_SECOND = 0.05; // Базовое количество нот в секунду (диапазон ±5)
 
 
-    private long globalTick = 0; // Поле для отслеживания глобального значения тика
+    private int globalTick = 0; // Поле для отслеживания глобального значения тика
 
     private int globalSampleIndex = 0;
     private Track track;
@@ -156,32 +156,52 @@ public class FakeMidiCreator
 
     private void createMidiFromAmplitudes(double[] amplitudes) throws Exception
     {
-
         int samplesPerSecond = 48000;
-        int sampleStep = samplesPerSecond / 2; // Берём амплитуду раз в 0.5 секунды
-        double secondsPerSample = 1.0 / samplesPerSecond;
-        int tickMultiplier = 480;
+        int sampleStep = samplesPerSecond / 2; // Раз в 0.5 секунды
+        int PPQ = 480;
+        double ticksPerSample = (double) (2*PPQ) / (samplesPerSecond * 3); // 0.01 тика на сэмпл
+
+        int currentTick = globalTick;
+
+        if (track.size() == 0)
+        {
+            track.add(createTempoEvent(120, 0)); // Устанавливаем темп
+        }
 
         for (int i = 0; i < amplitudes.length; i += sampleStep)
         {
             long sampleIndex = globalSampleIndex + i;
-            double timeInSeconds = sampleIndex * secondsPerSample;
-            int tick = (int) (timeInSeconds * tickMultiplier);
+            int tick = (int) (sampleIndex * ticksPerSample);
 
-            globalTick = tick; // Сохраняем глобальное значение тика
+            if (tick < currentTick)
+            {
+                tick = currentTick + PPQ;
+            }
+
+            currentTick = tick;
+            globalTick = tick;
 
             double amplitude = amplitudes[i] * 2.9;
             int velocity = Math.min((int) (amplitude * 127), 127);
-
             int note = 60 + (int) (amplitude * 30);
             note = Math.max(21, Math.min(108, note));
             velocity = Math.max(30, Math.min(127, velocity));
 
             notesCount++;
-            log("Добавлена нота: " + note + ", Velocity: " + velocity + ", Tick: " + tick);
             track.add(createMidiEvent(ShortMessage.NOTE_ON, 0, note, velocity, tick));
-            track.add(createMidiEvent(ShortMessage.NOTE_OFF, 0, note, 0, tick + 240));
+            track.add(createMidiEvent(ShortMessage.NOTE_OFF, 0, note, 0, tick + PPQ)); // Длина ноты = 1/4
         }
+    }
+
+
+
+    private MidiEvent createTempoEvent(int bpm, long tick) throws Exception
+    {
+        int tempo = 60000000 / bpm; // Формула для перевода BPM в микросекунды на четвертную ноту
+        MetaMessage message = new MetaMessage();
+        byte[] data = {(byte) (tempo >> 16), (byte) (tempo >> 8), (byte) tempo}; // 3 байта темпа
+        message.setMessage(0x51, data, 3);
+        return new MidiEvent(message, tick);
     }
 
     private static MidiEvent createMidiEvent(int command, int channel, int note, int velocity, int tick)
